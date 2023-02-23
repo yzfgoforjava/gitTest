aab12f4251ec01a649671cd20726d29d7038dbad
yzf <3160559243@qq.com>
2023-02-23 17:26:22 +0800

    other1 commit

--- src/main/java/mygit/HelloGit.java
+++ src/main/java/mygit/HelloGit.java
@@ -2,8 +2,9 @@ package mygit;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
